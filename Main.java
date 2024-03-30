23921fa26f267dda771be66368f7667393ab7e5e
Jonathan <jolm.020@outlook.com>
2024-03-29 21:52:03 -0600

    New update -- Menu implementation

--- Main.java
+++ Main.java
@@ -1,18 +1,96 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
         Deck deck = new Deck();
-        //Card card = new Card();
-
         deck.crearBaraja();
 
-        System.out.println(" ");
-        deck.shuffle();
-        System.out.println("--");
-        deck.head();
-        System.out.println("--");
-        deck.pick();
-        System.out.println("--");
-        deck.hand();
+        String repetir;
+        do {
+
+            int opc = 8;
+            boolean validar = true;
+
+
+            do{
+                System.out.println("Menu");
+                System.out.println("1. Mezclar deck");
+                System.out.println("2. Sacar una carta");
+                System.out.println("3. Carta al azar");
+                System.out.println("4. Generar una mano de 5 cartas");
+                System.out.println("5. Salir");
+
+                try {
+                    System.out.print("Opcion: ");
+                    Scanner scanner = new Scanner(System.in);
+                    opc = scanner.nextInt();
+                }catch (InputMismatchException e){
+                    System.out.println("Solo admite valores numericos");
+                }
+
+                if (opc >= 6 || opc <= 0) {
+                    System.out.println("Digite un numero entre los rangos correctos");
+                    validar = true;
+                } else {
+                    validar = false;
+                }
+
+
+            } while (validar);
+
+
+            switch (opc) {
+                case 1:
+                    System.out.println("1. Mezclar deck");
+                    deck.shuffle();
+
+                    break;
+                case 2:
+                    System.out.println("2. Sacar una carta");
+                    deck.head();
+
+                    break;
+                case 3:
+                    System.out.println("3. Carta al azar");
+                    deck.pick();
+
+                    break;
+                case 4:
+                    System.out.println("Generar una mano de 5 cartas");
+                    deck.hand();
+
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+
+            }
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("¿Deseas hacer algo mas? (s/n)");
+            repetir = scanner.nextLine();
+            repetir.toLowerCase();
+
+
+        } while (repetir.equals("s"));
+
     }
+
+//        Deck deck = new Deck();
+//        //Card card = new Card();
+//
+//        deck.crearBaraja();
+//
+//        System.out.println(" ");
+//        deck.shuffle();
+//        System.out.println("--");
+//        deck.head();
+//        System.out.println("--");
+//        deck.pick();
+//        System.out.println("--");
+//        deck.hand();
+
 }
